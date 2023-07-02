e640417f395fed2a15ac6ed21e59681602f8b193
kielsonzinn <kielsonzinn@gmail.com>
2023-07-02 14:50:01 -0300

    create acr-uncrustify

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
-# acr-extension-name
+# acr-uncrustify
+
+Extensao que verifica a identacao de arquivos usando a ferramenta uncrustify
+
+1. Propriedade config se refere ao caminho para o arquivo .cfg do uncrustify
 
 Arquivo config.json
 
 ```json
 {
-
+  "config": ""
 }
+```
+
+Dependencias
+
+- uncrustify
+
+```shell
+sudo apt-get install uncrustify
 ```
--- src/review.py
+++ src/review.py
@@ -1,18 +1,47 @@
-def review(config):
-    path_target = config['path_target']
-    path_source = config['path_source']
+import hashlib
+import os
+import re
+import subprocess
 
-    merge = config['merge']
-    project_id = merge['project_id']
-    merge_request_id = merge['merge_request_id']
-    
+
+def review(config):
+    path_source = config['path_source']
+    regex_file = config['regexFile']
+    arquivo_config = config['config']
+
     comments = []
-    
-    # TODO IMPLEMENTAR EXTENSION
-    #  O OBJETO DE COMENTARIO DEVE POSSUIR O SEGUINTE FORMATO
-    #  {
-    #      "id": "",
-    #      "comment": ""
-    #  }
+
+    for root, dirs, files in os.walk(path_source):
+        for file in files:
+            file_path = os.path.join(root, file)
+
+            if re.search(regex_file, file_path):
+                if not __verificar_indentacao_arquivo(file_path, arquivo_config):
+                    path_relative = file_path.replace(path_source, "")[1:]
+                    comments.append({
+                        "id": __generate_md5(file_path),
+                        "comment": f"Indentação incorreta no arquivo {path_relative}",
+                        "position": {
+                            "language": "c++",
+                            "path": path_relative,
+                            "startInLine": 1,
+                            "endInLine": 1,
+                            "snipset": False
+                        }
+                    })
 
     return comments
+
+
+def __verificar_indentacao_arquivo(arquivo, arquivo_config):
+    comando = ['uncrustify', '-c', arquivo_config, '--check', arquivo]
+    resultado = subprocess.run(comando, capture_output=True, text=True)
+
+    return resultado.returncode == 0
+
+
+def __generate_md5(string):
+    md5_hash = hashlib.md5()
+    md5_hash.update(string.encode('utf-8'))
+
+    return md5_hash.hexdigest()
